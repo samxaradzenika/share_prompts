ad34e0f22fe3ad8795e5baf6de6650632106baba
samxaradzenika <murdoc746@gmail.com>
2023-06-13 16:35:53 +0400

    started up create post page

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
@@ -9,12 +9,14 @@ export const connectToDB = async () => {
     console.log("MongoDB is already connected");
     return;
   }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
 
     console.log("MongoDB connected");
